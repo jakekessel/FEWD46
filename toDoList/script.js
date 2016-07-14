44c8331fbf88d3937d7d31a5dc4646f15fc066dc
Jake Kessel <jakekessel@gmail.com>
2016-07-14 12:45:07 -0700

    initial steps of coding it from pseudo code
    initial steps of coding it from pseudo code

--- toDoList/script.js
+++ toDoList/script.js
@@ -1,28 +1,69 @@
-// var form = document.querySelector("#new-item-form");
-//
-// function formSubmitted(event) {
-//   event.preventDefault();
-//
-//   // Your code goes here...
-//
-//   form.reset();
-// }
-//
-// form.addEventListener("submit", formSubmitted);
+var form = document.querySelector("#new-item-form");
+var input = document.querySelector("new-item-input")
+var ul =
 
-// html has form, input, button, list;
-// most of these will need to be referenced as variables;
-// button not needed as variable because?
-// link would use click event but form uses submit event;
-// why must eventlistener come after function?
+function formSubmitted(event) {
+  event.preventDefault();
 
-create form, input and list variables referencing html structure;
-create function that will run when form is submitted;
-function:
-create variables that create li label input and span elements within existing ul
-(label is parent of input and span but why is span not parent of input?)
-append the following:
-li to ul;
+  // Your code goes here...
+
+  form.reset();
+}
+
+form.addEventListener("submit", formSubmitted);
+//
+// psuedo-code first attempt:
+// // html has form, input, button, list;
+// // most of these will need to be referenced as variables;
+// // button not needed as variable because?
+// // link would use click event but form uses submit event;
+// // why must eventlistener come after function?
+//
+// =========================================================
+//
+// pseudo-code based on Elisas script (referenced as little as possible):
+//
+// create form, input and list variables referencing html structure;
+// create function that will run when form is submitted;
+// function:
+// create variables that create li label input and span elements within existing ul
+// (label is parent of input and span but why is span not parent of input?)
+// append the following:
+// li to ul;
+// label to li;
+// inout to label;
+// span to label;
+// set input field type (checkbox);
+// make text content of html/user input field the value of span;
+// reset form after each button submission;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
